5f4479e1d9e48aa4cbf63c5ca0ac095d2adb9076
Anirudh Vemula <anirudh.anirudh15@gmail.com>
2020-03-08 18:57:39 -0500

    feat: exercise 3 and assignment

--- cards/deck.go
+++ cards/deck.go
@@ -1,11 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 type deck []string
 
 func (d deck) print() {
-	for i, car := range d {
-		fmt.Println(i, car)
+	for i, card := range d {
+		fmt.Println(i, card)
 	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+
+	cardValues := []string{"Ace", "two", "three"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func readDeckFromFile(filename string) deck {
+	bs, error := ioutil.ReadFile(filename)
+
+	if error != nil {
+		fmt.Println("Error :", error)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- cards/main.go
+++ cards/main.go
@@ -4,18 +4,25 @@ import "fmt"
 
 func main() {
 	//var card string = "Ace of spades"
-	card := "Ace of Spades"
-	card = "King of Spades"
+	// card := "Ace of Spades"
+	// card = "King of Spades"
 
-	card2 := newCard()
+	// card2 := newCard()
 
-	cards := deck{"1 of Diamonds", "2 of Spades"}
-	cards = append(cards, "3 of Hearts")
-	fmt.Println(card)
-	fmt.Println(card2)
+	// cards := deck{"1 of Diamonds", "2 of Spades"}
+	// cards = append(cards, "3 of Hearts")
+	// fmt.Println(card)
+	// fmt.Println(card2)
 
+	cards := newDeck()
+	hand, remainingCards := deal(cards, 4)
+	hand.print()
+	remainingCards.print()
+
+	cards.saveToFile("allCards")
+	cards = readDeckFromFile("allCards")
+	fmt.Println("Printing cards from file")
 	cards.print()
-
 }
 
 func newCard() string {
